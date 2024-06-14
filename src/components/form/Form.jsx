27a99927436d9a94e01cba7b5cd9c87785c5bce1
Thien Nguyen <114895619+neitt0@users.noreply.github.com>
2024-06-14 02:35:49 -0400

    First Commitgit status

---
+++ src/components/form/Form.jsx
@@ -0,0 +1,9 @@
+function Form() {
+  return (
+    <>
+      <div className="test">hello world 2!</div>
+    </>
+  );
+}
+
+export default Form;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import App from "./App";
+import Form from "./components/form/Form";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
+    <Form />
   </React.StrictMode>
 );
 
